c0cacf6cb84c79f6c3cf56ea447377fdf0475335
Ksenia Krokhaleva <karibzhanova55@yandex.ru>
2023-03-22 21:33:44 +0500

    меняет кнопки на ссылки

--- source/js/main.js
+++ source/js/main.js
@@ -26,7 +26,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  scrollBtn.addEventListener('click', () => {
+  scrollBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
     mainForm.scrollIntoView({behavior: 'smooth'});
   });
 
